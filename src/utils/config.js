ab4c99a0b6eadcf9fcb35e86ea4da6e0bcda9d9c
Guilherme-Mascarenhas <guillherme.s.mascarenhas03@gmail.com>
2024-01-23 19:31:06 -0300

    first commit

---
+++ src/utils/config.js
@@ -0,0 +1,10 @@
+const config = {
+    development: {
+      apiEndpoint: 'http://localhost:3001/', // Substitua pelo seu endpoint de desenvolvimento
+    },
+    production: {
+      apiEndpoint: 'https://api.example.com', // Substitua pelo seu endpoint de produção
+    },
+  };
+  
+  export default config;
