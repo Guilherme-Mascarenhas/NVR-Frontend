ca9fa0659a4a9f3a61547f8a6c4fd2e4e31e42c3
Henrique <henrique@itfast.com.br>
2024-01-24 14:41:15 -0300

    updated commit

--- src/teste.js
+++ src/teste.js
@@ -1,47 +1,71 @@
-  import React, {useState,useEffect, useRef } from 'react';
-  import jsmpeg from 'jsmpeg';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
+import config from "./utils/config";
+const currentEnvironment = process.env.NODE_ENV || "development";
+const socketEndpoint = config[currentEnvironment].socketEndpoint;
 
-  function blobToDataURL(blob) {
-    return new Promise((resolve) => {
-      const reader = new FileReader();
-      reader.onloadend = () => resolve(reader.result);
-      reader.readAsDataURL(blob);
-    });
-  }
+function VideoPlayer() {
+	const [imagemURL, setImagemURL] = useState(null);
+	const websocket = useRef(null);
 
+	//init and desconnect of socket
+	useEffect(() => {
+		websocket.current = new WebSocket("ws://localhost:9999/");
+		websocket.current.onopen = () => {
+			console.log("Conexão WebSocket aberta");
+		};
 
-  function VideoPlayer() {
-    const [imagemURL, setImagemURL] = useState(null);
-    const websocket = useRef(null);
+		websocket.current.onmessage = (message) => {
+			const blob = message.data;
+			const url = URL.createObjectURL(blob);
 
-    useEffect(() => {
-      websocket.current = new WebSocket('ws://localhost:9999/');
-      websocket.current.onopen = () => {
-        console.log('Conexão WebSocket aberta');
-      };
-      
+			setImagemURL(url);
 
-      websocket.current.onmessage = (message) => {
-        const blob = message.data;
-        const url = URL.createObjectURL(blob);
-        // Atualizar o estado com o URL
-        setImagemURL(url);
-        websocket.current.close();
-        return () => URL.revokeObjectURL(url);
-        
-      }
+			return () => {
+				URL.revokeObjectURL(url);
+				if (websocket.current) {
+					console.log("WebSocket connection closed");
+					websocket.current.close();
+				}
+			};
+		};
+	}, []);
 
-    
-        // Limpar URL quando o componente for desmontado para evitar vazamentos de memória
-       
-      }, []);
+	/*const changeChannel = async (event) => {
+		let channel = event.target.value;
 
-      return (
-        <div>
-          {imagemURL && (
-            <img src={imagemURL} alt="Imagem" />
-          )}
-        </div>
-      );
-    };
-  export default VideoPlayer;
+		try {
+			const response = await axios.get(
+				socketEndpoint + `start-stream/${channel}`
+			);
+
+			if (!response.data.error) {
+				websocket.current = new WebSocket("ws://localhost:9999/");
+				websocket.current.onopen = () => {
+					console.log("Conexão WebSocket aberta");
+				};
+
+				websocket.current.onmessage = (message) => {
+					const blob = message.data;
+					const url = URL.createObjectURL(blob);
+
+					setImagemURL(url);
+				};
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};*/
+
+	return (
+		<div>
+			<select>
+				<option value="1">Canal 1</option>
+				<option value="2">Canal 2</option>
+			</select>
+			<img src={imagemURL} alt="Imagem" />
+		</div>
+	);
+}
+
+export default VideoPlayer;
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,11 @@
 const config = {
-    development: {
-      apiEndpoint: 'http://localhost:3001/', // Substitua pelo seu endpoint de desenvolvimento
-    },
-    production: {
-      apiEndpoint: 'https://api.example.com', // Substitua pelo seu endpoint de produção
-    },
-  };
-  
-  export default config;
+	development: {
+		apiEndpoint: "http://localhost:3001/",
+		socketEndpoint: "http://localhost:3010/", //  endpoint de desenvolvimento
+	},
+	production: {
+		apiEndpoint: "https://api.example.com", // endpoint de produção
+	},
+};
+
+export default config;
